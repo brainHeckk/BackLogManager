d874ee8829394446e3674bfacdfc221f7dc7f1de
unknown <gospodindjordjedjordjevic@gmail.com>
2025-04-18 20:02:02 +0200

    bukv nista

---
+++ app/src/main/java/com/example/blogger/Login.kt
@@ -0,0 +1,14 @@
+package com.example.blogger
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_screen)
+    }
+}
